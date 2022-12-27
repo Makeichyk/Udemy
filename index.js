9054ce6341bf7ff1aef36a0a2b9a77856b00e5f1
Jaroslav Makeichyk <jaroslau1@gmail.com>
2022-12-27 17:24:42 +0300

    feat: complite 1st task & add DB filling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let numbersOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numbersOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?", ""));
 
-let personalMovieDB = {
+const personalMovieDB = {
   count: numbersOfFilms,
   movies: {},
   actors: {},
   genres: [],
   private: false,
 };
+
+personalMovieDB.movies[
+  prompt("Один из последних фильмов что вы смотрели?", "")
+] = prompt("На сколько его оцените по 10-бальной системе?", "");
+personalMovieDB.movies[
+  prompt("Один из последних фильмов что вы смотрели?", "")
+] = prompt("На сколько его оцените по 10-бальной системе?", "");
