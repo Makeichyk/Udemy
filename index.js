43a76d98ee0fc48ce70c1d169a3bb7aea57f63e0
Jaroslav Makeichyk <jaroslau1@gmail.com>
2022-12-27 16:57:20 +0300

    feat: add script base

---
+++ index.js
@@ -0,0 +1,9 @@
+let numbersOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+
+let personalMovieDB = {
+  count: numbersOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
